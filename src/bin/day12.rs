f273a51e2a5f94b13887585a9b554bb0df845ca4
traxys <quentin+dev@familleboyer.net>
2023-12-12 22:23:44 +0100

    Optimize ranges with '.' in them

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -2,12 +2,14 @@ use std::time::Instant;
 
 use aoc_2023::{load, print_res};
 use bstr::{BString, ByteSlice};
+use cached::proc_macro::cached;
 use color_eyre::eyre::eyre;
-use indicatif::ParallelProgressIterator;
+//use indicatif::ParallelProgressIterator;
+use indicatif::ProgressIterator;
 use itertools::Itertools;
-use rayon::prelude::*;
+//use rayon::prelude::*;
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 pub enum State {
     Damaged,
     Operational,
@@ -66,9 +68,19 @@ pub fn parsing(input: &BString) -> color_eyre::Result<Parsed> {
         .collect()
 }
 
-fn possible_arrangements(springs: SpringField, ranges: &[usize]) -> usize {
+#[cached]
+fn possible_arrangements_segment(springs: Vec<State>, ranges: Vec<usize>) -> usize {
+    if springs.iter().all(|&s| s == State::Damaged) {
+        if ranges.len() == 1 && ranges[0] == springs.len() {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    let ranges = ranges.as_slice();
+
     let mut stack = vec![(0, None, ranges)];
-    let springs = &springs.0;
 
     let mut count = 0;
     while let Some((idx, prev, ranges)) = stack.pop() {
@@ -93,31 +105,93 @@ fn possible_arrangements(springs: SpringField, ranges: &[usize]) -> usize {
                     count += 1;
                 }
             } else if idx + len < springs.len() {
-                assert_ne!(springs[idx + len], State::Damaged,);
+                assert_ne!(springs[idx + len], State::Damaged);
 
                 stack.push((idx + len + 1, Some(State::Operational), &ranges[1..]));
             }
         }
 
         if springs[idx] != State::Damaged {
-            let next = springs
-                .iter()
-                .enumerate()
-                .skip(idx + 1)
-                .find(|&(_, &s)| s != State::Operational);
-            if let Some((next, _)) = next {
-                stack.push((next, Some(State::Operational), ranges));
-            }
+            stack.push((idx + 1, Some(State::Operational), ranges));
         }
     }
 
     count
 }
 
+fn possible_arrangements(springs: SpringField, ranges: &[usize]) -> usize {
+    //println!("\n\nStarting: {springs} ranges: {ranges:?}");
+
+    let springs = &springs.0;
+
+    let runs = springs
+        .split(|&s| s == State::Operational)
+        .filter(|r| !r.is_empty())
+        .collect_vec();
+
+    let mut remaining_ranges = vec![(im::Vector::new(), ranges, im::Vector::new())];
+
+    for &run in runs.iter() {
+        let could_fit = (run.len() + 1) / 2;
+
+        //println!(" run {} (could fit {could_fit})", SpringField(run.to_vec()));
+
+        let mut new_remaining_ranges = Vec::new();
+
+        for (possible, ranges, used) in &remaining_ranges {
+            //println!("  with ranges {ranges:?}");
+
+            if run.iter().all(|&s| s == State::Unknown) {
+                new_remaining_ranges.push((possible.clone(), *ranges, used.clone()));
+            }
+
+            for r in 1..=could_fit {
+                if r > ranges.len() {
+                    continue;
+                }
+
+                let (try_range, next_range) = ranges.split_at(r);
+
+                let ways = possible_arrangements_segment(run.to_vec(), try_range.to_vec());
+
+                /*println!(
+                    "   for (used:{used:?}) {try_range:?} (remain: {next_range:?}) --> {ways}"
+                );*/
+
+                if ways > 0 {
+                    let mut new_possible = possible.clone();
+                    new_possible.push_back(ways);
+                    let mut used = used.clone();
+                    used.push_back(try_range);
+
+                    new_remaining_ranges.push((new_possible, next_range, used));
+                }
+            }
+        }
+
+        // for (ways, range, used) in &new_remaining_ranges {
+        //     println!("  end: (used: {used:?}) {range:?} (ways: {ways:?})");
+        // }
+
+        remaining_ranges = new_remaining_ranges;
+    }
+
+    remaining_ranges.retain(|(_, r, _)| r.is_empty());
+
+    // for (ways, _, used) in &remaining_ranges {
+    //     println!(" final: {used:?} (ways: {ways:?})");
+    // }
+
+    remaining_ranges
+        .iter()
+        .map(|(possible, _, _)| possible.iter().product::<usize>())
+        .sum()
+}
+
 pub fn part1(input: Parsed) {
     let number_of_arrangements: usize = input
         .into_iter()
-        .map(|(s, r)| possible_arrangements(s, &r))
+        .map(|(s, r)| possible_arrangements(s.clone(), &r))
         .sum();
 
     print_res!("Total number of arragengements: {number_of_arrangements}");
@@ -141,8 +215,9 @@ pub fn part2(input: Parsed) {
 
     let line_count = inputs.len() as u64;
     let number_of_arrangements: usize = inputs
-        .into_par_iter()
-        .map(|(s, r)| possible_arrangements(s, &r))
+        .into_iter()
+        //.into_par_iter()
+        .map(|(s, r)| possible_arrangements(s.clone(), &r))
         .progress_count(line_count)
         .sum();
 
