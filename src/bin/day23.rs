bb271cbcc0c28a4f46be503492b04a3917aa997d
Quentin Boyer <quentin.boyer@eviden.com>
2023-12-24 19:28:39 +0100

    Day 23

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -1,9 +1,9 @@
-use std::{borrow::Cow, collections::HashMap, time::Instant};
+use std::{collections::HashMap, time::Instant};
 
 use aoc_2023::{load, print_res};
-use bitvec::{bitvec, vec::BitVec};
 use bstr::{BString, ByteSlice};
 use itertools::Itertools;
+use petgraph::{algo::all_simple_paths, prelude::*};
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 pub enum Cell {
@@ -106,47 +106,6 @@ fn longest_path(
     longest.unwrap() + 1
 }
 
-type DryCache<'a> = ahash::HashMap<(usize, usize, Cow<'a, BitVec>), Option<usize>>;
-
-fn longest_path_dry(
-    to_x: usize,
-    to_y: usize,
-    visited: &mut BitVec,
-    start: usize,
-    input: &[Vec<Cell>],
-    cache: &mut DryCache,
-) -> Option<usize> {
-    if (to_x, to_y) == (start, 0) {
-        return Some(0);
-    }
-
-    let mut longest = None;
-
-    for (nx, ny) in neighbours(to_x, to_y) {
-        let c = input[ny][nx];
-        let flat_idx = nx + ny * input[0].len();
-        if c != Cell::Forest && !visited[flat_idx] {
-            let mut visited = visited.clone();
-            visited.set(flat_idx, true);
-            let len = match cache.get(&(nx, ny, Cow::Borrowed(&visited))) {
-                Some(&l) => l,
-                None => {
-                    let v = longest_path_dry(nx, ny, &mut visited, start, input, cache);
-                    cache.insert((to_x, to_y, Cow::Owned(visited)), v);
-                    v
-                }
-            };
-            let Some(len) = len else { continue };
-            longest = match longest {
-                Some(old) => Some(std::cmp::max(old, len)),
-                None => Some(len),
-            };
-        }
-    }
-
-    longest.map(|x| x + 1)
-}
-
 pub fn part1(input: Parsed) {
     let (start, end) = ends(&input);
     let mut cache = HashMap::new();
@@ -159,18 +118,101 @@ pub fn part1(input: Parsed) {
     print_res!("Longest path: {longest_len}");
 }
 
+fn intersections(input: &Parsed) -> Vec<(usize, usize)> {
+    let mut points = Vec::default();
+
+    for (y, line) in input.iter().enumerate().skip(1).take(input.len() - 2) {
+        for (x, &c) in line.iter().enumerate().skip(1).take(input[0].len() - 2) {
+            if c != Cell::Forest
+                && neighbours(x, y)
+                    .into_iter()
+                    .map(|(x, y)| input[y][x])
+                    .filter(|&c| c != Cell::Forest)
+                    .count()
+                    >= 3
+            {
+                points.push((x, y));
+            }
+        }
+    }
+
+    points
+}
+
+fn first_intersection(
+    mut current: (usize, usize),
+    mut from: (usize, usize),
+    input: &Parsed,
+    intersections: &[(usize, usize)],
+) -> ((usize, usize), usize) {
+    let mut len = 1;
+    loop {
+        if intersections.contains(&current) {
+            return (current, len);
+        } else {
+            let ((nx, ny),) = neighbours(current.0, current.1)
+                .into_iter()
+                .filter(|&(x, y)| (x, y) != from && input[y][x] != Cell::Forest)
+                .collect_tuple()
+                .unwrap();
+            from = current;
+            current = (nx, ny);
+            len += 1;
+        }
+    }
+}
+
 pub fn part2(input: Parsed) {
     let (start, end) = ends(&input);
-    let mut cache = ahash::HashMap::default();
 
-    let mut set = bitvec![0; input[0].len() * input.len()];
-    let flat_idx = end + (input.len() - 1) * input[0].len();
-    set.set(flat_idx, true);
+    let intersections = intersections(&input);
 
-    let longest_len =
-        longest_path_dry(end, input.len() - 2, &mut set, start, &input, &mut cache).unwrap() + 1;
+    let (first, first_len) = first_intersection((start, 1), (start, 0), &input, &intersections);
+    let (last, last_len) = first_intersection(
+        (end, input.len() - 2),
+        (end, input.len() - 1),
+        &input,
+        &intersections,
+    );
 
-    print_res!("Longest path: {longest_len}");
+    let neigh_idx = |neighbour| {
+        intersections
+            .iter()
+            .enumerate()
+            .find(|&(_, &c)| c == neighbour)
+            .unwrap()
+            .0
+    };
+
+    let mut graph = UnGraphMap::new();
+
+    for (i, &(ix, iy)) in intersections.iter().enumerate() {
+        if (ix, iy) == first || (ix, iy) == last {
+            continue;
+        }
+
+        for (nx, ny) in neighbours(ix, iy)
+            .into_iter()
+            .filter(|&(x, y)| (x, y) != (ix, iy) && input[y][x] != Cell::Forest)
+        {
+            let (neighbour, dist) = first_intersection((nx, ny), (ix, iy), &input, &intersections);
+            let neighbour = neigh_idx(neighbour);
+
+            graph.add_edge(i, neighbour, dist);
+        }
+    }
+
+    let mut largest_len = 0;
+
+    for path in all_simple_paths::<Vec<_>, _>(&graph, neigh_idx(first), neigh_idx(last), 0, None) {
+        let len: usize = path
+            .windows(2)
+            .map(|w| *graph.edge_weight(w[0], w[1]).unwrap())
+            .sum();
+        largest_len = std::cmp::max(largest_len, len + first_len + last_len);
+    }
+
+    print_res!("Largest path possible: {largest_len}");
 }
 
 pub fn main() -> color_eyre::Result<()> {
